b8bb766967e78d6422b8bb16d5b6bd3e156f8839
Devin Garg <garg.7@iitj.ac.in>
2020-10-18 20:31:42 +0530

    five searches done

--- local.py
+++ local.py
@@ -1,60 +1,229 @@
 """
-    Contains implementations of local search functions
-    for a generalized problem containing following functions -
-    problem.getObjectiveFnValue()
-    problem.getNeighbours()
-    problem.getStartState()
-    problem.getRandomNeighbour()
-    problem.isBetter()
+    Implementations of local search functions for a generalized problem containing following functions -
+    --------
+        problem.getObjValue()
+        problem.getNeighbours()
+        problem.getRandomNeighbour()
+        problem.isBetter()
 """
 
-def hillClimbing_S(problem, state):
-    """
-        'Steepest Direction Hill Climbing'
 
-        Inputs: 
-            problem - A problem class with required functions
-            state - The state at which the problem is right now
+# ------- Deterministic Algorithms ------- #
+
+def hillClimbingSearch_S(problem, state):
+    """
+        `'Steepest Direction Hill Climbing'`
+
+        Inputs:
+        -------
+        `problem`
+            A problem class with required functions
+        `state`
+            The state at which the problem is right now
+        
         Output:
-            state - The state returned from Steepest Hill Climbing
+        -------
+        `state`
+            The state returned after Steepest Hill Climbing
     """
 
-    current_state = problem.getStartState()
-    current_obj_fn_value = problem.getObjectiveFnValue(current_state)
+    currentState = state
     while True:
-        neighbours = problem.getNeighbours(current_state)
-        best_neighbour = None
+        neighbours = problem.getNeighbours(currentState)
+        runningBest = currentState
         for n in neighbours:
-            if problem.isBetter(problem.getObjectiveFnValue(n), current_obj_fn_value):
-                best_neighbour = n
-        if best_neighbour:
-            current_state = best_neighbour
+            nObjVal = problem.getObjValue(n)
+            runningBestVal = problem.getObjValue(runningBest)
+            if problem.isBetter(nObjVal, runningBestVal):
+                runningBest = n
+        if runningBest is currentState:
+            # no neighbour is better, optimum reached
+            return currentState
         else:
-            return current_state
+            # jump to best neighbour
+            currentState = runningBest
 
-def hillClimbing_FC(problem, state, maxTrials=100):
+
+def tabuSearch(problem, state, tabuSize=32, maxSteps=-1):
     """
-        'First-Choice Hill Climbing'
+        `'Tabu Search'`
 
-        Inputs: 
-            problem - A problem class with required functions
-            state - The state at which the problem is right now
-            maxTrials (optional) - Number of random neighbours to try before giving up
+        Inputs:
+        -------
+        `problem`
+            A problem class with required functions
+        `state`
+            The state at which the problem is right now
+        `tabuSize` (optional)
+            Number of elements to be remembered in the Tabu Tenure
+        `maxSteps` (optional)
+            Maximum number of steps allowed. Default: -1 means keep going till an optimum.
+        
         Output:
-            state - The state returned from one step of First-Choice Hill Climbing
+        -------
+        `state`
+            The state returned after Tabu Search
     """
 
-    current_state = problem.getStartState()
-    current_obj_fn_value = problem.getObjectiveFnValue(current_state)
-    while True:
-        trials = 0
-        while trials < maxTrials:
-            neighbour = problem.getRandomNeighbour(current_state)
-            obj_fn_val = problem.getObjectiveFnValue(neighbour)
-            if problem.isBetter(obj_fn_val, current_obj_fn_value):
-                return neighbour
-            trials+=1
-        print(f"After {maxTrials} trials too, couldn't find a better neighbour for {problem.getInfo(current_state)} ")
-    return current_state
+    currentState = state
+    tabuList = list()
+    tabuList.add(currentState)
+    steps = 0
+    while maxSteps = -1 or steps<maxSteps:
+        runningBest = currentState
+        neighbours = problem.getNeighbours(currentState)
+        for n in neighbours:
+            runningBestVal = problem.getObjValue(runningBest)
+            nObjVal = problem.getObjValue(n)
+            if n not in tabuList and \
+                problem.isBetter(nObjVal, runningBestVal):
+                runningBest = n
+        
+        if bestNeighbour:
+            tabuList.append(bestNeighbour)
+            if len(tabuList) > tabuSize:
+                tabuList.pop(0)
+            currentState = bestNeighbour
+            steps+=1
+        else:
+            print(f"{steps} completed. Nowhere else to go.")
+            return currentState
+    return currentState
 
-def hillClimbingStep_RR(problem, state):
+
+# ------- Stochastic Algorithms ------- #
+
+def hillClimbingSearch_FC(problem, state, maxTrials=100):
+    """
+        `'First-Choice Hill Climbing'`
+
+        Inputs
+        ------ 
+        `problem`
+            A problem class with required functions
+        `state`
+            The state at which the problem is right now
+        `maxTrials` (optional)
+            Number of random neighbours to try before giving up.
+        
+        Output
+        ------
+        `state`
+            The state returned after First-Choice Hill Climbing
+    """
+
+    currentState = state
+    while True:
+        currentObjVal = problem.getObjValue(currentState)
+        trials = 0
+        betterState = None
+        while trials < maxTrials:
+            neighbour = problem.getRandomNeighbour(currentState)
+            nObjVal = problem.getObjValue(neighbour)
+            if problem.isBetter(nObjVal, currentObjVal):
+                betterState = neighbour
+                break
+            trials+=1
+        if betterState:
+            # jump to neighbour better than current state
+            currentState = betterState
+        else:
+            # trials exhausted, no better neighbour found
+            print(f"Even after {maxTrials} trials too, couldn't find a better neighbour for {problem.getInfo(currentState)} ")
+            return currentState
+
+
+def hillClimbingStep_RR(problem, state, p=0.1, maxSteps=10_000):
+    """
+        `'Random-Restart Hill Climbing'`
+
+        Inputs
+        ------ 
+        `problem`
+            A problem class with required functions
+        `state`
+            The state at which the problem is right now
+        `p` (optional)
+            The probability of random restart
+        `maxSteps` (optional)
+            Maximum number of steps allowed. Default = 10,000
+        
+        Output
+        ------
+        `state`
+            The state returned after Random-Restart Hill Climbing
+    """
+
+    import random
+    currentState = state
+    steps = 0
+    bestYet = currentState
+    while steps<maxSteps:
+        currentObjVal = problem.getObjValue(currentState)
+        if random.random()>p:
+            # make a greedy step
+            neighbours = problem.getNeighbours(currentState)
+            runningBest = currentState
+            for n in neighbours:
+                nObjVal = problem.getObjValue(n)
+                runningBestVal = problem.getObjValue(runningBest)
+                if problem.isBetter(nObjVal, runningBestVal):
+                    runningBest = n
+            if runningBest is currentState:
+                # no neighbour is better, check against the bestYet
+                runningBestVal = problem.getObjValue(runningBest)
+                bestYetVal = problem.getObjValue(bestYet)
+                if problem.isBetter(runningBestVal, bestYetVal):
+                    bestYet = runningBest
+            else:
+                # jump to best neighbour
+                currentState = runningBest
+        else:
+            # do a random restart
+            currentState = problem.getRandomState()
+    # after running out of steps, return the best yet state
+    return bestYet
+
+
+def simulatedAnnealing(problem, state, schedule):
+    """
+        `'Random-Restart Hill Climbing'`
+
+        Inputs
+        ------ 
+        `problem`
+            A problem class with required functions
+        `state`
+            The state at which the problem is right now
+        `schedule`
+            The schedule according to which temperature varies.
+        
+        Output
+        ------
+        `state`
+            The state returned after Simulated Annealing
+    """
+    
+    import random
+    from math import exp
+    
+    currentState = state
+    for i in range(len(schedule)):
+        temperature = schedule[i]
+        if temperature = 0:
+            return currentState
+        neighbour = problem.getRandomNeighbour(currentState)
+        changeInObj = problem.getObjValue(neighbour) - \
+                        problem.getObjValue(currentState)
+        if changeInObj > 0:
+            # if the neighbour is better, jump
+            currentState = neighbour
+        else:
+            # if the neighbour is worse, jump with some probability
+            if exp(changeInObj/temperature) >= random.random():
+                currentState = neighbour
+    return currentState
+
+def geneticAlgorithm(problem, state):
+    #TODO
+    pass
