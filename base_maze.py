4d609a01463ce6a591017108fbc61510fe97250a
Devin Garg <garg.7@iitj.ac.in>
2020-10-18 16:59:50 +0530

    updated the maze

--- base_maze.py
+++ base_maze.py
@@ -92,11 +92,21 @@ for f in fList:
 
 
 class GameState:
-    def __init__(self, walls, food, pLoc, qLoc):
+    def __init__(self, walls, food, pLoc, qLoc, height, width):
         self.walls = walls
         self.food = food
         self.pLoc = pLoc
         self.qLoc = qLoc
+        self.pEaten = 0
+        self.qEaten = 0
+        self.height = height
+        self.width = width
+
+    def isGameOver(self):
+        if True not in food:
+            return True
+        else:
+            return False
 
 
 def getSuccessors(state, turn='p'):
@@ -113,16 +123,69 @@ def getSuccessors(state, turn='p'):
         exit(2)
     possible = []
     if walls[x-1][y] == False and (xe!=x-1 or ye!=y):
-        possible.append(((x, y+1), 'North'))
+        possible.append(((x-1, y), 'North'))
     if walls[x][y+1] == False and (ye!=y+1 or xe!=x):
-        possible.append(((x+1, y+2), 'East'))
+        possible.append(((x, y+1), 'East'))
     if walls[x][y-1] == False and (ye!=y-1 or xe!=x):
-        possible.append(((x+1, y), 'West'))
+        possible.append(((x, y-1), 'West'))
     if walls[x+1][y] == False and (xe!=x+1 or ye!=y):
-        possible.append(((x+2, y+1), 'South'))
-    print(possible)
-    return "bYe"
+        possible.append(((x+1, y), 'South'))
+    # print(possible)
+    return possible
 
-state = GameState(walls, food, p_init, q_init)
-getSuccessors(state, 'q')
+def getMaze(state):
+    walls = state.walls
+    food = state.food
+    h = state.height
+    w = state.width
+    pLoc = state.pLoc
+    qLoc = state.qLoc
+    for row in range(height):
+        for col in range(w):
+            if walls[row][col]==True:
+                print('#',end='')
+            elif food[row][col]==True:
+                print('.',end='')
+            elif pLoc == (row, col):
+                print('P',end='')
+            elif qLoc == (row, col):
+                print('Q',end='')
+            else: print(' ',end='')
+        print()
 
+
+state = GameState(walls, food, p_init, q_init, height, width)
+# getSuccessors(state, 'q')
+# getMaze(state)
+
+
+def movePlayer(state, action, player):
+    if state.food[action[0][0]][action[0][1]]==True:
+        state.food[action[0][0]][action[0][1]]=False
+        if player=='p':
+            state.pEaten += 1
+        else:
+            state.qEaten += 1
+    
+    if player=='p':
+        state.pLoc = action[0]
+    else:
+        state.qLoc = action[0]
+
+    return
+
+i=0
+
+while i<10:
+    move = getSuccessors(state, 'p')
+    if len(move)!=0:
+        movePlayer(state, move[-1], 'p')
+    getMaze(state)
+    move = getSuccessors(state, 'q')
+    if len(move)!=0:
+        movePlayer(state, move[-1], 'q')
+    getMaze(state)
+    i+=1
+    if len(getSuccessors(state, 'p'))==0 and \
+        len(getSuccessors(state, 'q'))==0:
+        break
